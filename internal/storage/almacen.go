0550ab33607bb4ce618694bd1adac4a752203633
Nahim Simba <nahimsimba0810@gmail.com>
2026-06-11 09:07:23 -0500

    feat: add user operations to interface

--- internal/storage/almacen.go
+++ internal/storage/almacen.go
@@ -8,6 +8,12 @@ type Almacen interface {
 	// MODULO 1
 	// Users
 	ListarUsers() []models.User
+	BuscarUserPorID(id int) (models.User, bool)
+	CrearUser(u models.User) models.User
+	ActualizarUser(id int, datos models.User) (models.User, bool)
+	BorrarUser(id int) bool
+
+	//
 
 	// MODULO 2
 
