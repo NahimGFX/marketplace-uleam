730721978d3dbbdd1edf3525247d2b7c3e8184bf
DAKA-PL <daivelyn1107pincay@gmail.com>
2026-06-11 11:06:42 -0500

    feat: add CRUD operations for categories, products, and orders

--- internal/storage/almacen.go
+++ internal/storage/almacen.go
@@ -16,6 +16,26 @@ type Almacen interface {
 	//
 
 	// MODULO 2
+	// Categorias
+	ListarCategorias() []models.Categoria
+	BuscarCategoriaPorID(id int) (models.Categoria, bool)
+	CrearCategoria(c models.Categoria) models.Categoria
+	ActualizarCategoria(id int, datos models.Categoria) (models.Categoria, bool)
+	BorrarCategoria(id int) bool
+
+	// Productos
+	ListarProductos() []models.Producto
+	BuscarProductoPorID(id int) (models.Producto, bool)
+	CrearProducto(p models.Producto) models.Producto
+	ActualizarProducto(id int, datos models.Producto) (models.Producto, bool)
+	BorrarProducto(id int) bool
+
+	// Ordenes
+	ListarOrdenes() []models.Orden
+	BuscarOrdenPorID(id int) (models.Orden, bool)
+	CrearOrden(o models.Orden) models.Orden
+	ActualizarOrden(id int, datos models.Orden) (models.Orden, bool)
+	BorrarOrden(id int) bool
 
 	// MODULO 3
 	ListarMessages() []models.Message
