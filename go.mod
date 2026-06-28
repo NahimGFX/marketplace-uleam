bb7bce8c5c36b387c883147f2af9b9b4d4cb0ea4
jostin05-ap <jostinalexander05@gmail.com>
2026-06-28 01:53:33 -0500

    feat: add layered tests (service, handler, repository) with mocks, httptest and GORM sqlite memory

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.26.2
 require github.com/go-chi/chi/v5 v5.3.0
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/glebarez/go-sqlite v1.22.0 // indirect
 	github.com/glebarez/sqlite v1.11.0 // indirect
@@ -13,10 +14,13 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
 	golang.org/x/crypto v0.53.0 // indirect
 	golang.org/x/sys v0.46.0 // indirect
 	golang.org/x/text v0.38.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/gorm v1.31.2 // indirect
 	modernc.org/libc v1.37.6 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
---
+++ internal/storage/message_gorm_test.go
@@ -0,0 +1,55 @@
+package storage_test
+
+import (
+	"testing"
+
+	"marketplace-api/internal/models"
+
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+)
+
+func setupDBTest(t *testing.T) *gorm.DB {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		t.Fatalf("no se pudo abrir la base de datos en memoria: %v", err)
+	}
+
+	db.AutoMigrate(&models.Message{})
+	return db
+}
+
+func TestMessage_Crear_Buscar_Listar(t *testing.T) {
+	db := setupDBTest(t)
+
+	// CREAR
+	message := models.Message{Content: "hola mundo"}
+	db.Create(&message)
+
+	if message.ID == 0 {
+		t.Fatalf("esperaba ID generado")
+	}
+
+	// BUSCAR
+	var encontrado models.Message
+	err := db.First(&encontrado, message.ID).Error
+	if err != nil {
+		t.Fatalf("no se pudo encontrar el mensaje")
+	}
+
+	if encontrado.Content != "hola mundo" {
+		t.Fatalf("contenido incorrecto")
+	}
+
+	// LISTAR
+	var mensajes []models.Message
+	db.Find(&mensajes)
+
+	if len(mensajes) != 1 {
+		t.Fatalf("esperado 1 mensaje, obtenido %d", len(mensajes))
+	}
+
+	if mensajes[0].Content != "hola mundo" {
+		t.Fatalf("dato incorrecto en lista")
+	}
+}
